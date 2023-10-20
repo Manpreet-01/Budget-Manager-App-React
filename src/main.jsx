cf82046aaf915ef418b7159da594e7f02205bda7
Manpreet Singh <manpreetmss400@gmail.com>
2023-10-20 08:33:41 +0530

    initial setup

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App.jsx'
+
+// import './components/index.css'
+// import {BrowserRouter} from "react-router-dom";
+
+//<BrowserRouter>
+//</BrowserRouter>
+
+ReactDOM.createRoot (
+  document.getElementById('root')
+).render (
+  <React.StrictMode>
+    
+      <App />
+    
+  </React.StrictMode>,
+);
+
+
